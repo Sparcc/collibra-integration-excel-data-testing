238a6688184c1f0497efcc3377254ca7789a3147
avih8r <sparco.209@gmail.com>
2018-04-13 13:10:18 +1000

    Put code in a class

--- read-xslx-test.py
+++ read-xslx-test.py
@@ -2,40 +2,62 @@ from openpyxl import load_workbook
 import unittest
 import logging
 import csv
-class compareExcelFiles(unittest.TestCase):
+
+class compareExcel:
+    file1Cols = []
+    file2Cols = []
+    def __init__(self, file1, file2, debugging = False):
+        self.debugging = debugging
+        self.wb = {}
+        self.ws = {}
+        self.wb["file1"] = load_workbook(filename = file1)
+        self.ws["file1"] = self.wb["file1"].active
+        self.wb["file2"] = load_workbook(filename = file2)
+        self.ws["file2"] = self.wb["file2"].active
+    def setDataLength(self, upperRange):
+        #if (
+        #    self.dataLength = 
+        self.upperRange = upperRange
+    def compareFiles(self):
+        #see first row
+        #if first cell (heading) matches file1Cols[i] then firstColFile1=Cell collm
+        #for now columns must be manually identified, two coll
+        firstCol = {}
+        secondCol = {}
+        i=2
+        while i<self.upperRange:
+            firstCol["file1"] = self.ws["file1"][self.file1Cols[0]+str(i)].value
+            secondCol["file1"] = self.ws["file1"][self.file1Cols[1]+str(i)].value
+            firstCol["file2"] = self.ws["file2"][self.file2Cols[0]+str(i)].value
+            secondCol["file2"] = self.ws["file2"][self.file2Cols[1]+str(i)].value
+            if (self.debugging):
+                print(firstCol["file1"]+", ")
+                print(firstCol["file2"]+"\n")
+                print("------------------")
+                print(secondCol["file1"]+", ")
+                print(secondCol["file2"]+"\n")
+            assert(firstCol["file1"] == firstCol["file2"])
+            assert(secondCol["file1"] == secondCol["file2"])
+            i+=1
+        
+    def setColumns(self, file1Cols, file2Cols):
+        for x in file1Cols:
+            self.file1Cols.append(x)
+            
+        for x in file2Cols:
+            self.file2Cols.append(x)
+
+class testcompareExcelFiles(unittest.TestCase):
     def setUp(self):
-        logging.warning('setUp not tested')
-    def test_compareImportWithExport(self):
-        debugging = False
-        wb = load_workbook(filename = 'collibra-tute-test-data.xlsx')
-        ws=wb.active
-        wb2 = load_workbook(filename = 'Default.xlsx')
-        ws2=wb2.active
-        i=2 #ignoring headings
-        rowString=""
-        #ofile  = open('custom-testdata.csv', "w", newline='')
-        #writer = csv.writer(ofile)
-        firstCol = ["",""]
-        secondCol = ["",""]
-        while i<51:
-            if (debugging):
-                rowString=""
-                rowString+=ws['A'+str(i)].value
-                rowString+=", "
-                rowString+=ws['C'+str(i)].value
-                rowString+="--Compared To Export From Collibra-->"
-                rowString+=ws2['A'+str(i)].value
-                rowString+=", "
-                rowString+=ws2['C'+str(i)].value
-                print(rowString)
-            firstCol[0] = ws['A'+str(i)].value
-            firstCol[1] = ws2['A'+str(i)].value
-            secondCol[0] = ws['C'+str(i)].value
-            secondCol[1] = ws2['C'+str(i)].value
-            self.assertEqual(firstCol[0],firstCol[1])
-            self.assertEqual(secondCol[0],secondCol[1])
-            i+= 1
-    def tearDown(self):
-        logging.warning('tearDown not tested')
+        self.file1 = {"name":"collibra-tute-test-data.xlsx"}
+        self.file2 = {"name":"Default.xlsx"}
+        self.comparer = compareExcel(self.file1["name"], self.file2["name"])
+        self.comparer.setDataLength(52)
+    def test_compareImportWithExport1(self):
+        file1 = ['A','C']
+        file2 = ['A','C']
+        self.comparer.setColumns(file1, file2)
+        self.comparer.compareFiles()
+    #def tearDown(self):
 if __name__ == '__main__':
     unittest.main()
