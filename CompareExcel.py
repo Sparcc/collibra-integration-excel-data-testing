3d531c25350d0e11a5f083470c844c335b6a444a
avih8r <sparco.209@gmail.com>
2018-05-31 09:20:55 +1000

    added (multiple)one to many mapping and streamlined processing

--- CompareExcel.py
+++ CompareExcel.py
@@ -4,31 +4,31 @@ import logging
 import csv
 
 class CompareExcel:
-    file1Cols = []
-    file2Cols = []
+    destColumns = []
+    srcColumns = []
     numCols = 0
     wb = {}
     ws = {}
     testPassed = True
-    def __init__(self, file1 = "", file2 = "", debugging = False):
+    def __init__(self, dest = "", src = "", debugging = False):
         self.debugging = debugging
         if debugging:
-            if file1 == "" or file2 == "":
+            if dest == "" or src == "":
                 logging.warning('files not set!')
             else:
-                self.setSourceFiles(file1,file2)
-    def setSourceFiles(self, file1, file2):
-        self.wb["file1"] = load_workbook(filename = file1)
-        self.ws["file1"] = self.wb["file1"].active
-        self.wb["file2"] = load_workbook(filename = file2)
-        self.ws["file2"] = self.wb["file2"].active
+                self.setSourceFiles(dest,src)
+    def setSourceFiles(self, dest, src):
+        self.wb["dest"] = load_workbook(filename = dest)
+        self.ws["dest"] = self.wb["dest"].active
+        self.wb["src"] = load_workbook(filename = src)
+        self.ws["src"] = self.wb["src"].active
     def setDataLength(self, upperRange):
         #TODO: Add code to calculate upperRange of both files and see if they are different
         self.upperRange = upperRange
     def compareFiles(self, startingRange = 2): #0 is null, 1 is heading
         rowNum=startingRange
         while rowNum<self.upperRange:
-            #compare file1 row and file2 row for each col
+            #compare dest row and src row for each col
             self.processRow(rowNum)
             rowNum+=1
     def processRow(self,rowNum):
@@ -36,18 +36,18 @@ class CompareExcel:
         row = {}
         colNum = 0
         while colNum < self.numCols:
-            row["file1"] = self.ws["file1"][self.file1Cols[colNum]+str(rowNum)].value
-            row["file2"] = self.ws["file2"][self.file2Cols[colNum]+str(rowNum)].value
-            self.compareData(row["file1"],row["file2"],rowNum)
+            row["dest"] = self.ws["dest"][self.destColumns[colNum]+str(rowNum)].value
+            row["src"] = self.ws["src"][self.srcColumns[colNum]+str(rowNum)].value
+            self.compareData(row["dest"],row["src"],rowNum)
             colNum+=1
     def compareData(self,d1,d2,rowNum):
         if d1 != d2:
             self.testPassed = False
             print('MISMATCH! [{v1}] does not match [{v2}] on rowNum={rn}'.format(v1=d1,v2=d2,rn=rowNum))
-    def setColumns(self, file1Cols, file2Cols):
-        for x in file1Cols:
-            self.file1Cols.append(x)
+    def setColumns(self, destColumns, srcColumns):
+        for x in destColumns:
+            self.destColumns.append(x)
             
-        for x in file2Cols:
-            self.file2Cols.append(x)
-        self.numCols = len(file1Cols)
+        for x in srcColumns:
+            self.srcColumns.append(x)
+        self.numCols = len(destColumns)
--- read-xslx-test.py
+++ read-xslx-test.py
@@ -5,7 +5,7 @@ from openpyxl import load_workbook
 import unittest
 
 class CompareExcelMapping(CompareExcel):
-    def buildMap(self):
+    def buildMaps(self):
         self.mapping={}
         self.mapping['schema'] = ['R','G']
         self.mapping['size'] = ['P','H']
@@ -15,62 +15,53 @@ class CompareExcelMapping(CompareExcel):
         self.mapping['default_value'] = ['I','L']
         self.mapping['desc'] = ['E','M']
         self.mapping['pk'] = ['N','N']
+        
+        #define maps
+        self.oneToManyMapping = []
+        #1st mapping
+        self.oneToManyMapping.append({})
+        self.oneToManyMapping[0]['concat'] = 'A'
+        self.oneToManyMapping[0]['separate'] = {}
+        self.oneToManyMapping[0]['separate'] = {'schema':'B','table':'C','column':'F'}
     def verifyMapRow(self,rowNum):
         for k,v in self.mapping.items():
-            d1 = self.ws["file1"][v[0]+str(rowNum)].value
-            d2 = self.ws["file2"][v[1]+str(rowNum)].value
-    def buildConcatColMapAndVerifyRow(self,rowNum):
-        #define maps
-        concatCol = 'A'#column to split
-        f1_schema = 0 #1st part of split column
-        f1_table = 1
-        f1_column = 2
-        
-        f2_schema = 'B' #column B
-        f2_table = 'C'
-        f2_column = 'F'
-
+            d1 = self.ws["dest"][v[0]+str(rowNum)].value
+            d2 = self.ws["src"][v[1]+str(rowNum)].value
+    def verifyOneToManyMapRow(self,rowNum):
         #process schema,table,column
-        row = {} #stores in here for code readability
-        row["file1"] = self.ws["file1"][concatCol+str(rowNum)].value.split('::')
-        
-        #schema
-        row["file2"] = self.ws["file2"][f2_schema+str(rowNum)].value
-        self.compareData(row["file1"][f1_schema],row["file2"],rowNum)
-        
-        #table
-        row["file2"] = self.ws["file2"][f2_table+str(rowNum)].value
-        self.compareData(row["file1"][f1_table],row["file2"],rowNum)
-        
-        #column
-        row["file2"] = self.ws["file2"][f2_column+str(rowNum)].value
-        self.compareData(row["file1"][f1_column],row["file2"],rowNum)
+        for map in self.oneToManyMapping:
+            row = {} #stores in here for code readability
+            row["dest"] = self.ws["dest"][map['concat']+str(rowNum)].value.split('::')
+            subCol=0
+            for k,v in map['separate'].items():
+                row["src"] = self.ws["src"][v+str(rowNum)].value
+                self.compareData(row["dest"][subCol],row["src"],rowNum)
+                subCol+=1
     def processRow(self,rowNum):
-
-        self.buildConcatColMapAndVerifyRow(rowNum)
+        self.verifyOneToManyMapRow(rowNum)
         self.verifyMapRow(rowNum)            
 
 class testcompareExcelIntegrationData(unittest.TestCase):
     def setUp(self):
-        #self.comparer = CompareExcel(file1, file2)
+        #self.comparer = CompareExcel(dest, src)
         self.comparer = CompareExcel()
         self.comparer.setDataLength(52)
         
         self.oracleComparer = CompareExcelMapping()
         self.oracleComparer.setDataLength(36)
     def testCompareExcel(self):
-        file1 = "CompareExcel-test-file1.xlsx"
-        file2 = "CompareExcel-test-file2.xlsx"
-        self.comparer.setSourceFiles(file1, file2)
-        file1 = ['A','C','D']
-        file2 = ['A','C','F']
-        self.comparer.setColumns(file1, file2)
+        dest = "CompareExcel-test-file1.xlsx"
+        src = "CompareExcel-test-file2.xlsx"
+        self.comparer.setSourceFiles(dest, src)
+        dest = ['A','C','D']
+        src = ['A','C','F']
+        self.comparer.setColumns(dest, src)
         self.comparer.compareFiles()
     def testBIDW_Excel_calendar_table(self):
-        file1 = "oracle-collibra-export-calendar.xlsx"
-        file2 = "oracle-export-calendar.xlsx"
-        self.oracleComparer.setSourceFiles(file1, file2)
-        self.oracleComparer.buildMap()
+        dest = "oracle-collibra-export-calendar.xlsx"
+        src = "oracle-export-calendar.xlsx"
+        self.oracleComparer.setSourceFiles(dest, src)
+        self.oracleComparer.buildMaps()
         self.oracleComparer.compareFiles(startingRange = 2)
         self.assertEqual(True, self.oracleComparer.testPassed)
     #def tearDown(self):
